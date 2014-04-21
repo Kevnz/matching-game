198a1d970b2573feec73eb05a6adfac2d812a2cd
elliothath <elliothathaway@hotmail.com>
2014-04-22 11:02:53 +1200

    Added power gem
    Added the power gem functionality
    If there is a power gem match then all of matched colour gems are
    removed from the board

--- js/game/entities/grid.js
+++ js/game/entities/grid.js
@@ -5,6 +5,7 @@ define(['crafty', 'game/game','game/entities/brick', 'sift'],
         return Crafty.e("Delay").delay(callback, delayTime, 0);
     };
 	var isMove = false;
+    var insertedPowers = [];
 	const ANIMATION_SPEED = Game.animation_speed;
 	var rows = Game.rows;
 	var cols = Game.columns;
@@ -82,14 +83,19 @@ define(['crafty', 'game/game','game/entities/brick', 'sift'],
  		}
 	};
 
-var remove = function (brick) {
+var remove = function (brick, insertPowerGem) {
 	//if(!brick.gridX) return;
 	var x = brick.gridX;
 	var y = brick.gridY;
+    var locationHash = getLocationHash(brick);
+
+    if(insertedPowers[locationHash]) return;
+
 	try{
         if(gameGrid[y][x] === null) return;
 
 		  gameGrid[y][x] = null;
+          if(insertPowerGem) gameGrid[y][x] = Brick.fadeIn('power', x, y);
 		  brick.bind('TweenEnd', function(){
 		  	brick.destroy();
 		  	Crafty.trigger('scored', 10);
@@ -101,6 +107,18 @@ var remove = function (brick) {
 		console.log('errBrick', brick);
 	}
 };
+
+var removeColor = function (colorToRemove) {
+    var row,column;
+    for (column = 0; column < gameGrid.length; column++) {
+        for (row = 0; row < gameGrid[column].length; row++) {
+            var ent = gameGrid[column][row];
+            if(ent && ent.color === colorToRemove) {
+                remove(ent, false);
+            }
+        }
+    }
+};
 var isEmptyBelow = function (brick) {
 
 }
@@ -110,13 +128,45 @@ var siftingScan = function () {
    var results = simpleScan();//not really simple;
 
     var foundMatch = results.length > 0;
+    insertedPowers = [];
     for(var j = 0; j < results.length; j++) {
-        remove(results[j]);
+        
+        var matchGroup = results[j];
+        var hasPower = false;
+        var groupColor = '';
+        var insertPower = matchGroup.length >= 5;
+        var insertAt = Math.floor(matchGroup.length / 2);
+        var locationHash = getLocationHash(matchGroup[insertAt]);
+        if(insertPower){
+            if(insertedPowers[locationHash]){
+                insertPower = false;
+            }
+        }
+
+        for (var i = 0; i < matchGroup.length; i++){
+            if(matchGroup[i].color === 'power'){
+                hasPower = true;
+            }else{
+                groupColor = matchGroup[i].color;
+            }
+            remove(matchGroup[i], (insertPower && i==insertAt));
+        }
+        if(insertPower){
+            insertedPowers[locationHash] = true;
+        }
+            
+        if(hasPower){
+            removeColor(groupColor);
+        }
     }
 
     return foundMatch;
 
 };
+
+var getLocationHash = function(brick){
+    return brick.gridX +',' + brick.gridY;
+}
 
 var goLower = function (column, row) {
    // console.log('goLower');
@@ -249,34 +299,32 @@ var simpleScan = function () {
         var matches = [];
         for(var i = 0; i< brickss.length; i++) {
             var brick = brickss[i];
-            console.log(i, brick.color);
 
-            if(brick.color === currentColor) {
+            if(currentColor === 'power') {
+                currentColor = brick.color;
+            }
+
+            if(brick.color === currentColor || brick.color === 'power') {
                 tempMatch.push(brick);
             }
             else{
                 if(tempMatch.length > 2){
                     matches.push(tempMatch.slice(0));
+                    tempMatch = [brick];
+                } else if(tempMatch[tempMatch.length-1].color === 'power'){
+                    tempMatch = [tempMatch[tempMatch.length-1], brick];
+                } else{
+                    tempMatch = [brick];
                 }
+
                 currentColor = brick.color;
-                tempMatch = [brick];
             }
         }
         if(tempMatch.length > 2){
             matches.push(tempMatch.slice(0));
         }
 
-        //return matches;
-        console.log('SIFTBRICKS RESULTS:', matches.length);
-        console.log(brickss);
-
-        var matches1d = [];
-
-        for(var i = 0; i< matches.length; i++) {
-            matches1d = matches1d.concat(matches[i]);
-        }   
-
-        return matches1d;
+        return matches;
     };
 
 
@@ -284,20 +332,13 @@ var simpleScan = function () {
 
     for(var i = 0; i< cols; i++) {
         var myCol = getCol(i);
-        //console.log(myCol);
-        console.log('Matches on col:', i);
         var tempResults = siftBricks(myCol);
-        //console.log(tempResults);
         results = results.concat(tempResults);
     }
 
     for(var j = 0; j< rows; j++) {
-       //results = results.concat(siftBricks(getRow(i)));
         var myRow = getRow(j);
-        //console.log(myCol);
-        console.log('Matches on row:', j);
         var tempResultsRow = siftBricks(myRow);
-        //console.log(tempResults);
         results = results.concat(tempResultsRow);
     }
     console.log('RESULTS:::');
